233b8354302105a3cb9e8361578694185b7de2e5
Nathan Arthur <nathan@nathanarthur.com>
2024-03-29 14:20:18 -0400

    use baserow for queueSetScheds

--- backend/src/lib/queueSetScheds.ts
+++ backend/src/lib/queueSetScheds.ts
@@ -1,40 +1,15 @@
 "use strict";
-import _ from "lodash/fp";
 import { queueSetSched } from "./queueSetSched";
-import { USERS_TABLE_NAME } from "../constants";
-import { Callback } from "../types";
-import { doc } from "serverless-dynamodb-client";
+import { BASEROW_TABLE } from "../constants";
+import { DbUser } from "../types";
+import getBaserow from "./services/baserow/getBaserow";
 
-export const queueSetScheds = (evt: unknown, ctx: unknown, cb: Callback) => {
-  doc
-    .scan({
-      TableName: USERS_TABLE_NAME,
-      Select: "SPECIFIC_ATTRIBUTES",
-      ProjectionExpression: "#n", // 'name' is a reserved word in DDB
-      ExpressionAttributeNames: { "#n": "name" },
-    })
-    .promise()
-    .then(
-      (res: {
-        LastEvaluatedKey?: unknown;
-        Items?: Record<string, unknown>[];
-      }) => {
-        if (res.LastEvaluatedKey !== undefined) {
-          cb(
-            "Scan needed more than 1 page of results! Echo, go implement this.",
-            null
-          );
-        } else {
-          const promises = _.map(
-            (item: { name: unknown }) => queueSetSched(item.name),
-            res.Items
-          );
-          Promise.all(promises).then(
-            (res) => cb(null, res),
-            (err) => cb(err, null)
-          );
-        }
-      },
-      (err: unknown) => cb(err, null)
-    );
+export const queueSetScheds = async () => {
+  const baserow = getBaserow();
+  const { results: users } = await baserow.listRows<DbUser>(
+    BASEROW_TABLE.users
+  );
+  const promises = users.map((user) => queueSetSched(user.username));
+
+  await Promise.all(promises);
 };
